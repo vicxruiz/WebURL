68beb4df22ccf95c5e03bb29fb98cd4024fa8262
Victor Ruiz <ruizdvictor@gmail.com>
2020-12-27 15:54:39 -0500

    first commit

---
+++ WebURLTests/WebURLTests.swift
@@ -0,0 +1,33 @@
+//
+//  WebURLTests.swift
+//  WebURLTests
+//
+//  Created by Victor Ruiz on 12/27/20.
+//
+
+import XCTest
+@testable import WebURL
+
+class WebURLTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
